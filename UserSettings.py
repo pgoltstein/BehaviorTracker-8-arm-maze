4baa3cadaa716756bb97f308af5f4c93c8653d64
Pieter Goltstein <pgoltstein@mac.com>
2017-05-10 18:34:46 +0200

    added settings file

---
+++ UserSettings.py
@@ -0,0 +1,19 @@
+####
+###
+## Settings for BehaviorTracker.py
+#
+
+# Names of mice to be used in experiment
+mouse_names = ["Tst1","Tst2"]
+
+# Duration of a single trial
+trial_duration = 1 # minute
+
+# Location where to store data
+data_path = "/data/track"
+
+# Position of the program on the computer monitor
+screen_pos = (200,100) # (x,y)
+
+# Resolution of the acquired video
+video_resolution = (640,480) # (x,y)
