d765799fdf65eabaaee2ae8b76b207c2754a796a
Pieter Goltstein <pgoltstein@mac.com>
2017-05-15 13:22:27 +0200

    updated tracking, storing of arm, calculation of occupancy, colors

--- BehaviorTracker.py
+++ BehaviorTracker.py
@@ -46,81 +46,83 @@ class MainWindow():
         self.main = main
         self.main.title("Behavior Tracker")
 
-        # Create mouse selector menu
+        # Create mouse selector menu (row=10)
         self.mouse_name = tk.StringVar()
         self.mouse_name.set(settings.mouse_names[0])
-        self.mouse_label = tk.Label(self.main, text="Mouse: ")
-        self.mouse_label.grid(row=0,column=0)
+        tk.Label(self.main, text="Mouse: ").grid(row=10,sticky=tk.E)
         self.mouse_menu = tk.OptionMenu( self.main,
                                 self.mouse_name,*settings.mouse_names)
         self.mouse_menu.config(width=10)
-        self.mouse_menu.grid(row=0,column=1,columnspan=2)
+        self.mouse_menu.grid(row=10,column=1,columnspan=2,sticky=tk.W)
 
-        self.trial_dur_label = tk.Label(self.main, text="Trial duration: ")
-        self.trial_dur_label.grid(row=1,column=0)
+        tk.Label(self.main, text="Trial duration: ").grid(row=11,sticky=tk.E)
         self.trial_dur_entry = tk.Entry( self.main )
         self.trial_dur_entry.insert(0,str(settings.trial_duration))
         self.trial_dur_entry.config(width=5)
-        self.trial_dur_entry.grid(row=1,column=1)
+        self.trial_dur_entry.grid(row=11,column=1)
         self.minute_label = tk.Label(self.main, text="min.")
-        self.minute_label.grid(row=1,column=2)
+        self.minute_label.grid(row=11,column=2,sticky=tk.W)
 
-        # Preview
+        # Preview (row=20)
+        tk.Frame(height=2, width=300, bd=1, bg="#aaaaaa",
+            relief=tk.SUNKEN).grid(columnspan=3,pady=10)
         self.preview_button = tk.Button(self.main, text="Preview",
             fg="black", command=self.preview)
-        self.preview_button.grid(row=3,column=0)
+        self.preview_button.grid(column=1, pady=5)
 
-        # Template
-        self.template_label = tk.Label(self.main, text="Template")
-        self.template_label.grid(row=4,column=0)
+        # Template (row=30)
+        tk.Frame(height=2, width=300, bd=1, bg="#aaaaaa",
+            relief=tk.SUNKEN).grid(columnspan=3,pady=10)
+        tk.Label(self.main, text="Template").grid(sticky=tk.E)
         self.set_template_button = tk.Button(self.main, text="Set",
             fg="black", command=self.set_template)
-        self.set_template_button.grid(row=5,column=0)
+        self.set_template_button.grid(row=30,column=0)
         self.show_template_button = tk.Button(self.main, text="Show",
             fg="black", command=self.show_template)
-        self.show_template_button.grid(row=5,column=1)
+        self.show_template_button.grid(row=30,column=1)
 
-        # Maze location
-        self.maze_label = tk.Label(self.main, text="Maze")
-        self.maze_label.grid(row=6,column=0)
+        # Maze coordinates (row=40)
+        tk.Frame(height=2, width=300, bd=1, bg="#aaaaaa",
+            relief=tk.SUNKEN).grid(columnspan=3,pady=10)
+        tk.Label(self.main, text="Maze coordinates").grid(column=0)
         self.set_maze_center_button = tk.Button(self.main, text="Center",
             fg="black", command=self.set_maze_center)
-        self.set_maze_center_button.grid(row=7,column=0)
+        self.set_maze_center_button.grid(row=40,column=0)
         self.set_maze_radius_button = tk.Button(self.main, text="Radius",
             fg="black", command=self.set_maze_radius)
-        self.set_maze_radius_button.grid(row=7,column=1)
+        self.set_maze_radius_button.grid(row=40,column=1)
         self.set_maze_arm1_button = tk.Button(self.main, text="Arm 1",
             fg="black", command=self.set_maze_arm1)
-        self.set_maze_arm1_button.grid(row=7,column=2)
+        self.set_maze_arm1_button.grid(row=40,column=2)
         self.set_maze_save_button = tk.Button(self.main, text="Save",
             fg="black", command=self.set_maze_save)
-        self.set_maze_save_button.grid(row=8,column=2)
+        self.set_maze_save_button.grid(row=41,column=2)
 
-        # Tracking
-        self.tracking_label = tk.Label(self.main, text="Tracking")
-        self.tracking_label.grid(row=9,column=0)
+        # Tracking (row=50)
+        tk.Frame(height=2, width=200, bd=1, bg="#aaaaaa",
+            relief=tk.SUNKEN).grid(columnspan=3,pady=10)
+        tk.Label(self.main, text="Tracking").grid(column=0)
         self.start_tracking_button = tk.Button(self.main, text="Start",
             fg="black", command=self.track_mouse)
-        self.start_tracking_button.grid(row=10,column=0)
+        self.start_tracking_button.grid(row=50,column=0)
         self.test_tracking_button = tk.Button(self.main, text="Test",
             fg="black", command=self.test_tracking)
-        self.test_tracking_button.grid(row=10,column=1)
+        self.test_tracking_button.grid(row=50,column=1)
         self.remaining_label = tk.Label(self.main, text="Remaining")
-        self.remaining_label.grid(row=11,column=0)
+        self.remaining_label.grid(row=60,column=0,sticky=tk.E)
         self.rem_time_label = tk.Label(self.main, text="")
-        self.rem_time_label.grid(row=11,column=1)
+        self.rem_time_label.grid(row=60,column=1,sticky=tk.W)
 
-        # Quit
-        self.spacing_label = tk.Label(self.main, text=" ")
-        self.spacing_label.grid(row=12,column=2)
+        # Quit (row=100)
+        tk.Frame(height=20).grid(columnspan=3,pady=10)
         self.quit_button = tk.Button(self.main,
             text="Quit", fg="red", command=self.quit)
-        self.quit_button.grid(row=13,column=2)
+        self.quit_button.grid(row=100,column=2,pady=5,sticky=tk.E)
 
         # Set main window position
         self.main.update()
         w_main = self.main.winfo_width()
-        h_main = 400
+        h_main = 550
         self.main.geometry('{}x{}+{}+{}'.format(w_main, h_main,
             settings.screen_pos[0], settings.screen_pos[1]))
 
@@ -189,7 +191,12 @@ class MainWindow():
             arm_ang = ((arm*np.pi)/4) + arm1_ang
             x = int(maze_center[0] + (arm1_radius * np.sin(arm_ang)))
             y = int(maze_center[1] + (arm1_radius * np.cos(arm_ang)))
-            cv2.arrowedLine(image, maze_center, (x,y), (255,0,0), 0)
+            plot_col = (127-(127*np.sin(arm_ang)),127,127-(127*np.cos(arm_ang)))
+            cv2.arrowedLine(image, maze_center, (x,y), plot_col, 2)
+            x = int(maze_center[0] + (arm1_radius*0.7 * np.sin(arm_ang)))
+            y = int(maze_center[1] + (arm1_radius*0.7 * np.cos(arm_ang)))
+            cv2.putText( image, str(arm+1), (x,y), cv2.FONT_HERSHEY_SIMPLEX,
+                2, (0,0,0), thickness=3 )
 
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -266,25 +273,46 @@ class MainWindow():
             (settings.video_resolution[0],settings.video_resolution[1]) )
 
         # Do the tracking
-        timestamps,positions = self.perform_tracking(video_object=video_object)
+        timestamps,positions,arm = self.perform_tracking(video_object=video_object)
 
         # Release video file
         video_object.release()
 
+        # Analyze arm accupancy
+        last_time = timestamps[0]
+        arm_occupancy = np.zeros(9)
+        for t,a in enumerate(arm):
+            arm_occupancy[a] = arm_occupancy[a] + (timestamps[t]-last_time)
+            last_time = timestamps[t]
+
         # Save the timestamps and positions
         info_file_name = os.path.join(exp_path,'TrackingInfo')
+        occ_file_name = os.path.join(exp_path,'RelativeOccupancy.csv')
         tracker_info = {}
         tracker_info['timestamps'] = timestamps
         tracker_info['positions'] = positions
+        tracker_info['arm'] = arm
+        tracker_info['arm_occupancy'] = arm_occupancy
+        tracker_info['relative_occupancy'] = arm_occupancy/arm_occupancy.sum()
         np.save(info_file_name, tracker_info)
+        np.savetxt(occ_file_name, arm_occupancy/arm_occupancy.sum(),
+            fmt="%0.5f", delimiter=",")
 
     def test_tracking(self):
-        timestamps,positions = self.perform_tracking(video_object=None)
+        timestamps,positions,arm = self.perform_tracking(video_object=None)
         t0 = timestamps[0]
-        for t,p in zip(timestamps,positions):
-            print("{}: {}".format(int(1000*(t-t0)),p))
+        for t,p,a in zip(timestamps,positions,arm):
+            print("{}: {}, {}".format(int(1000*(t-t0)),p, a))
 
     def perform_tracking(self, video_object=None):
+
+        # Calculate list with arm angles
+        arm_angles = np.zeros(8)
+        arm1_ang = np.arctan2( maze_arm1[0]-maze_center[0],
+                               maze_arm1[1]-maze_center[1] )
+        for arm in range(8):
+            arm_angles[arm] = ((arm*np.pi)/4) + arm1_ang
+
         # Start window and move to correct position
         cv2.imshow('Preview',np.zeros( (settings.video_resolution[0],
                                     settings.video_resolution[1]), dtype=int ))
@@ -296,7 +324,9 @@ class MainWindow():
 
         # Capture, track and show frame-by-frame
         mouse_pos = []
+        mouse_arm = []
         timestamps = []
+
         max_time = (float(self.trial_dur_entry.get()) * 60) + time.time()
         while time.time() < max_time:
 
@@ -312,10 +342,27 @@ class MainWindow():
             frame_bw = template-frame_bw
             frame_bw = frame_bw/frame_bw.max()
 
-            # Find and draw mouse
+            # Find mouse
             (_,__,___,max_loc) = cv2.minMaxLoc(frame_bw)
             mouse_pos.append(max_loc)
-            cv2.circle(frame, max_loc, 10, (0,0,255), 1)
+            mouse_angle = np.pi + np.arctan2( max_loc[0]-maze_center[0],
+                                              max_loc[1]-maze_center[1] )
+            mouse_radius = np.sqrt( ((maze_center[0]-max_loc[0])**2) + \
+                                    ((maze_center[1]-max_loc[1])**2) )
+
+            # Classify mouse positions
+            if mouse_radius < maze_radius:
+                # Mouse = center
+                mouse_arm.append(0)
+                plot_col = (255,255,255)
+            else:
+                mouse_arm.append( ((np.argmin( np.abs(arm_angles-mouse_angle) )+4) %8) +1 )
+                plot_col = (127-(127*np.sin(mouse_angle)),127,127-(127*np.cos(mouse_angle)))
+
+            # Draw mouse
+            cv2.circle(frame, maze_center, 10, plot_col, 2)
+            cv2.circle(frame, max_loc, 10, plot_col, 2)
+            cv2.line(frame, maze_center, max_loc, plot_col, 2)
 
             # Show image and mouse
             cv2.imshow('Preview',frame)
@@ -327,7 +374,7 @@ class MainWindow():
 
         # Close window and return timestamps and positions
         cv2.destroyAllWindows()
-        return timestamps,mouse_pos
+        return timestamps,mouse_pos,mouse_arm
 
     def quit(self):
         self.cap.release()
