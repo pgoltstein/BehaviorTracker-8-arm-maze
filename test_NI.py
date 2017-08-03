7a3d019c9680f97171b22b152d602c0d0071f290
Pieter Goltstein <pgoltstein@mac.com>
2017-08-03 14:29:41 +0200

    got NI-USB6008 to work with python (using object oriented approach)

--- test_NI.py
+++ test_NI.py
@@ -3,5 +3,27 @@
 """
 Created on Thu Aug 3, 2017
 
+Simple example of digital output.
+This example outputs the values of data on line 0 to 7
+
 @author: pgoltstein
 """
+
+from PyDAQmx import *
+import numpy
+
+analog_input = Task()
+read = int32()
+data = numpy.zeros((1000,), dtype=numpy.float64)
+
+# DAQmx Configure Code
+analog_input.CreateAIVoltageChan("Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
+analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000)
+
+# DAQmx Start Code
+analog_input.StartTask()
+
+# DAQmx Read Code
+analog_input.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+
+print("Acquired {} points".format(read.value))
