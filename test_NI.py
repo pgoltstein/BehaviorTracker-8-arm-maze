6278ce847487d7f5a0bf7b54d5a4922e5408fd05
Pieter Goltstein <pgoltstein@mac.com>
2017-08-03 15:14:37 +0200

    digital writing using usb6008

--- test_NI.py
+++ test_NI.py
@@ -12,18 +12,26 @@ This example outputs the values of data on line 0 to 7
 import PyDAQmx
 import numpy
 
-analog_input = PyDAQmx.Task()
-read = PyDAQmx.int32()
-data = numpy.zeros((1000,), dtype=numpy.float64)
+# analog_input = PyDAQmx.Task()
+digital_output = PyDAQmx.Task()
+data = numpy.zeros((1,), dtype=numpy.uint8) + 4
+# read = PyDAQmx.int32()
+# data = numpy.zeros((1000,), dtype=numpy.float64)
 
 # DAQmx Configure Code
-analog_input.CreateAIVoltageChan("Dev1/ai0","",PyDAQmx.DAQmx_Val_Cfg_Default,-10.0,10.0,PyDAQmx.DAQmx_Val_Volts,None)
-analog_input.CfgSampClkTiming("",10000.0,PyDAQmx.DAQmx_Val_Rising,PyDAQmx.ArrayDAQmx_Val_FiniteSamps,1000)
+digital_output.CreateDOChan( "Dev1/port0/line0:7", None, PyDAQmx.DAQmx_Val_ChanForAllLines )
+# analog_input.CreateAIVoltageChan(
+#     "Dev1/ai0","",PyDAQmx.DAQmx_Val_Cfg_Default,-10.0,10.0,PyDAQmx.DAQmx_Val_Volts,None)
+# analog_input.CfgSampClkTiming("",10000.0,PyDAQmx.DAQmx_Val_Rising,PyDAQmx.DAQmx_Val_FiniteSamps,1000)
 
 # DAQmx Start Code
-analog_input.StartTask()
+digital_output.StartTask()
+# analog_input.StartTask()
 
 # DAQmx Read Code
-analog_input.ReadAnalogF64(1000,10.0,PyDAQmx.DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+samples_written = digital_output.WriteDigitalU8( \
+    8, True, 0.1, PyDAQmx.DAQmx_Val_GroupByScanNumber, data, None, None )
+# analog_input.ReadAnalogF64(1000,10.0,PyDAQmx.DAQmx_Val_GroupByChannel,data,1000,PyDAQmx.byref(read),None)
 
-print("Acquired {} points".format(read.value))
+print("Wrote {} samples".format(samples_written))
+# print("Acquired {} points".format(read.value))
