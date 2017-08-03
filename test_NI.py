7279a25b89631141557088e412f437b7b3634bff
Pieter Goltstein <pgoltstein@mac.com>
2017-08-03 14:45:42 +0200

    got NI-USB6008 to work with python (using object oriented approach)

--- test_NI.py
+++ test_NI.py
@@ -9,21 +9,21 @@ This example outputs the values of data on line 0 to 7
 @author: pgoltstein
 """
 
-from PyDAQmx import *
+import PyDAQmx
 import numpy
 
-analog_input = Task()
-read = int32()
+analog_input = PyDAQmx.Task()
+read = PyDAQmx.int32()
 data = numpy.zeros((1000,), dtype=numpy.float64)
 
 # DAQmx Configure Code
-analog_input.CreateAIVoltageChan("Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
-analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000)
+analog_input.CreateAIVoltageChan("Dev1/ai0","",PyDAQmx.DAQmx_Val_Cfg_Default,-10.0,10.0,PyDAQmx.DAQmx_Val_Volts,None)
+analog_input.CfgSampClkTiming("",10000.0,PyDAQmx.DAQmx_Val_Rising,PyDAQmx.ArrayDAQmx_Val_FiniteSamps,1000)
 
 # DAQmx Start Code
 analog_input.StartTask()
 
 # DAQmx Read Code
-analog_input.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+analog_input.ReadAnalogF64(1000,10.0,PyDAQmx.DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
 
 print("Acquired {} points".format(read.value))
